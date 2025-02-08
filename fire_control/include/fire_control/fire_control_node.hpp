6f202fb20a9bd51b24c206b396e4e96bed2a9d99
ouyyyyang <20234001019@m.scnu.edu.cn>
2025-02-08 17:29:43 +0800

    2.8

--- fire_control/include/fire_control/fire_control_node.hpp
+++ fire_control/include/fire_control/fire_control_node.hpp
@@ -10,6 +10,7 @@
 #include <tf2_ros/transform_listener.h>
 
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_ros/message_filter.h>  
 #include <rclcpp/rclcpp.hpp>
 #include <std_srvs/srv/trigger.hpp>
 #include <rclcpp/subscription.hpp>
@@ -37,24 +38,20 @@ public:
   
 private:
 
-  void TargetCallback(const std::shared_ptr<const auto_aim_interfaces::msg::Target> msg);
+  void TargetCallback(const auto_aim_interfaces::msg::Target::SharedPtr msg);
 
   void TimerCallback();
 
   std::unique_ptr<Solver> solver_;
-
-  message_filters::Subscriber<auto_aim_interfaces::msg::Target> aim_sub_;
-
-  std::optional<auto_aim_interfaces::msg::Target> latest_target_msg_; 
-
+  std::shared_ptr<auto_aim_interfaces::msg::Target> latest_target_msg_; 
   rclcpp::Publisher<fire_control_interfaces::msg::GimbalCmd>::SharedPtr gimbal_pub_;
-
   rclcpp::TimerBase::SharedPtr pub_timer_;
-
-  bool debug_mode_;
-
   std::shared_ptr<tf2_ros::Buffer> tf2_buffer_;
-
+  std::shared_ptr<tf2_ros::TransformListener> tf2_listener_;  
+  message_filters::Subscriber<auto_aim_interfaces::msg::Target> target_sub_;  
+  std::shared_ptr<tf2_ros::MessageFilter<auto_aim_interfaces::msg::Target>> tf2_filter_;  
+  std::string target_frame_; 
+  //bool debug_mode_;
 };
 
   
--- fire_control/src/fire_control_node.cpp
+++ fire_control/src/fire_control_node.cpp
@@ -24,10 +24,22 @@ FireControlNode::FireControlNode(const rclcpp::NodeOptions & options)
   auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
     this->get_node_base_interface(), this->get_node_timers_interface());
   tf2_buffer_->setCreateTimerInterface(timer_interface);
+
+  tf2_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf2_buffer_);
   
-  //  target subscribe
-  aim_sub_.subscribe(this, "/tracker/target", rclcpp::SensorDataQoS().get_rmw_qos_profile());
-  aim_sub_.registerCallback(std::bind(&FireControlNode::TargetCallback, this, std::placeholders::_1));
+  //  target subscribe and fliter
+  target_sub_.subscribe(this, "/tracker/target", rmw_qos_profile_sensor_data);
+  target_frame_ = this->declare_parameter("target_frame", "odom");
+  tf2_filter_ = std::make_shared<tf2_ros::MessageFilter<auto_aim_interfaces::msg::Target>>(target_sub_,
+                                             *tf2_buffer_,
+                                             target_frame_,
+                                             10,
+                                             this->get_node_logging_interface(),
+                                             this->get_node_clock_interface(),
+                                             std::chrono::duration<int>(1));
+  // Register a callback with tf2_ros::MessageFilter to be called when
+  // transforms are available
+  tf2_filter_->registerCallback(&FireControlNode::TargetCallback, this);
 
   gimbal_pub_ = this->create_publisher<fire_control_interfaces::msg::GimbalCmd>(
     "fire_control/cmd_gimbal",rclcpp::SensorDataQoS());
@@ -35,12 +47,18 @@ FireControlNode::FireControlNode(const rclcpp::NodeOptions & options)
     std::chrono::milliseconds(4), std::bind(&FireControlNode::TimerCallback, this));
 
   //debug_mode_ = this->declare_parameter<bool>("debug_mode", false); //之后用于发布markers
-    
+  RCLCPP_INFO(this->get_logger(), "FireControlNode initialization completed!"); 
 }
 
-void FireControlNode::TargetCallback(const std::shared_ptr<const auto_aim_interfaces::msg::Target> msg)  
+void FireControlNode::TargetCallback(const auto_aim_interfaces::msg::Target::SharedPtr msg)  
 {  
-  latest_target_msg_ = *msg; 
+  RCLCPP_INFO(this->get_logger(),   
+    "Target received - frame_id: %s, tracking: %d, stamp: %.3f",  
+    msg->header.frame_id.c_str(),  
+    msg->tracking,  
+    rclcpp::Time(msg->header.stamp).seconds());  
+  
+  latest_target_msg_ = msg; 
 }  
 
 void FireControlNode::TimerCallback()  
@@ -53,9 +71,14 @@ void FireControlNode::TimerCallback()
     return;  
   }  
 
-  auto target_msg = *latest_target_msg_; 
+  if (!tf2_buffer_->canTransform(  
+        "odom", "camera_optical_frame",  
+        tf2::TimePointZero)) {  
+        RCLCPP_WARN(this->get_logger(), "Required transform is not available");  
+        return;  
+    }
 
-  if(target_msg.header.frame_id.empty())
+  if(latest_target_msg_->header.frame_id.empty())
   {
     control_msg.yaw_diff = 0;  
     control_msg.pitch_diff = 0;  
@@ -69,9 +92,9 @@ void FireControlNode::TimerCallback()
 
  
  
-  if (target_msg.tracking) {  
+  if (latest_target_msg_->tracking) {  
     try {  
-      control_msg = solver_->Solve(target_msg, this->now(), tf2_buffer_);  
+      control_msg = solver_->Solve(*latest_target_msg_, this->now(), tf2_buffer_);  
     } catch (const std::runtime_error &e) {  
       RCLCPP_ERROR(this->get_logger(), "Runtime error in solver: %s", e.what());  
       control_msg.yaw_diff = 0;  
